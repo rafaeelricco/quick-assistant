225df1196bb0007eaa94ca02a9511ecba1e6c50d
rafaeelricco <rafaelricco10@gmail.com>
2025-11-13 15:53:56 -0300

    Refactor base classes to use Pydantic for improved immutability and type safety

--- src/common/base.py
+++ src/common/base.py
@@ -5,9 +5,53 @@ This module provides fundamental building blocks for immutable data structures
 used throughout the application, ensuring consistency and type safety.
 """
 
-from dataclasses import dataclass
+from pydantic import BaseModel, ConfigDict
+from common.json import FromJSON, ToJSON
 
-@dataclass(frozen=True)
-class BaseFrozen:
-    """Base class for immutable data structures."""
+
+class BaseFrozen(BaseModel):
+    """
+    A frozen base model class
+    """
+    model_config = ConfigDict(
+        strict=True,                    # no type coercion
+        frozen=True,                    # make immutable
+        arbitrary_types_allowed=False,  # properties that don't inherit from BaseModel
+        extra="forbid"                  # disallow extra fields
+    )
+
+class BaseMutable(BaseModel):
+    """
+    A mutable base model class
+    """
+    model_config = ConfigDict(
+        strict=True,                    # no type coercion
+        frozen=False,                   # make mutable
+        arbitrary_types_allowed=False,  # properties that don't inherit from BaseModel
+        extra="forbid"                  # disallow extra fields
+    )
+
+class BaseMutableArbitrary(BaseModel):
+    """
+    A mutable base model class
+    """
+    model_config = ConfigDict(
+        strict=True,                    # no type coercion
+        frozen=False,                   # make mutable
+        arbitrary_types_allowed=True,   # properties that don't inherit from BaseModel
+        extra="forbid"                  # disallow extra fields
+    )
+
+class BaseFrozenArbitrary(BaseModel):
+    """
+    A mutable base model class
+    """
+    model_config = ConfigDict(
+        strict=True,                    # no type coercion
+        frozen=True,                    # make mutable
+        arbitrary_types_allowed=True,   # properties that don't inherit from BaseModel
+        extra="forbid"                  # disallow extra fields
+    )
+
+class BaseSerializable(BaseFrozen, FromJSON, ToJSON):
     pass
